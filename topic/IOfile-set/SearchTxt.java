0f7726da153e4d7b9ba62f1fe82dc62f656af875
JontCont <information9527@gmail.com>
2020-01-29 23:29:32 +0800

    update 108/01/29

--- topic/IOfile-set/SearchTxt.java
+++ topic/IOfile-set/SearchTxt.java
@@ -1,26 +1,22 @@
 import java.io.*;
-import java.util.*;
+
 public class SearchTxt
 {
     public static void main(String[] args) throws IOException 
     {
         int i;
-        String Type_Name[] = {" "};
-        FileReader fr = new FileReader("ALLfile\\Name.txt");
+        String Type_Name[] =new String[5];
+        FileReader fr = new FileReader("ALLfile/Name.txt");
         BufferedReader br = new BufferedReader(fr);
-        //Scanner src = new Scanner(System.in);
-        //String sName = src.nextLine();
-        //int input = src.nextInt();
         while(br.ready())
         {
             for (i = 0; i <7; i++)
             {
                 Type_Name[i]=br.readLine();
-                System.out.println(Type_Name[1]);
-
+                if(Type_Name[i]==null) break;
+                System.out.println(Type_Name[i]);
             }
         }
-        //src.close();
         fr.close();
     }//main
 }
--- topic/diamond.java
+++ topic/diamond.java
@@ -1,5 +1,65 @@
+import java.util.Scanner;
+
 class diamond{
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        Scanner src = new Scanner(System.in);
+        String use_menu;
+        diamond dia = new diamond();
+
+        do{
+            System.out.println(">> A. 左三角形 <<");
+            System.out.println(">> B. 倒左三角形 <<");
+            System.out.println(">> C. 右三角形 <<");
+            System.out.println(">> D. 菱形角形 <<");
+            System.out.println(">> E. 正三角形 <<");
+            System.out.println(">> 0. 離開     <<");
+            use_menu = src.next();
+            switch(use_menu.toUpperCase())
+            {
+                case "A":dia.diamod_type01(); break;
+                case "B":dia.diamod_type02();break;
+                case "C":dia.diamod_type03();break;
+                case "D":dia.diamod_type04();break;
+                case "E":dia.diamod_type05();break;
+                case "0":System.out.print("exit ...");break;
+                default:System.out.println("ENTER EORRO !!!\n");
+            }
+        }while(!use_menu.equals("0"));
+        src.close();
+    }
+
+//-------------------sub-------------------------//
+    void diamod_type01()
+    {
+    int i,j;
+    for(i=0;i<5;i++,System.out.println())
+        for(j=0;j<i+1;j++)
+            System.out.print("*");
+            System.out.println();
+    }
+
+    void diamod_type02()
+    {
+        int i,j;
+        for(i=0;i<5;i++,System.out.println())
+            for(j=i;j<5;j++)
+                System.out.print("*");
+                System.out.println();
+    }
+
+    void diamod_type03(){
+        int i,j,k;
+        System.out.println();
+        for(i=0;i<5;i++,System.out.println())
+        {
+            for(j=0;j<5-i;j++)
+                System.out.print(" ");
+            for(k=0;k<i+1;k++)
+                System.out.print("*");
+        }
+    }
+    void diamod_type04(){
         int i ,j,k;
         for(i=0;i<5;i++)
         {
@@ -18,6 +78,9 @@ class diamond{
             System.out.println();
         }
         System.out.println();
+    }
+    void diamod_type05(){
+        int i,j,k;
         for(i=0;i<=5;i++)
         {
             for(j=0;j<5-i;j++)
@@ -26,7 +89,8 @@ class diamond{
                 System.out.print(" *");
             System.out.println();
         }
-        
+        System.out.println();
     }
+    //----------------------------//
 }
 
